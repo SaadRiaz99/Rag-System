ff9cbe26702d593b07a1eaad3f8c186997e3df0b
Saad Bin Riaz <malik.saad.riaz.96@gmail.com>
2026-08-19 15:57:23 +0500

    Fix filename and encoding in load_document function; update document handling

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 
 import csv
-import document.
-filename = "Document.csv"
+import document
+filename = "document/Documenr.csv"
 
 def load_document():
-    with open (filename , "r" , encoding=UnicodeTranslateError) as file:
-        csv.load(file)
+    with open (filename , "r" , encoding="utf-8") as file:
+        data = csv.DictReader(file)
+
+        return list(data)
+loader = load_document()
+def doumentcsv():
+    for loader in document:
+         print(document["title"])
+    print(document["content"])
+    print()
+        
